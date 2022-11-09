fd484990957e0d8d16adf19d7bdcafa52828858a
gealsanchez <gealsanchez@gmail.com>
2022-11-09 14:32:43 -0500

    To do list - structure

--- src/index.js
+++ src/index.js
@@ -1,21 +1,72 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './img/firefox.svg';
+import imgplus from './images/plus.svg';
+import imgreload from './images/reload.svg';
+import imgtrash from './images/trash.svg';
 
-function component() {
-  const element = document.createElement('div');
+const reloadDiv = document.querySelector('.reload');
+const myReload = new Image();
+myReload.src = imgreload;
+reloadDiv.appendChild(myReload);
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const plusDiv = document.querySelector('.plus');
+const myPlus = new Image();
+myPlus.src = imgplus;
+plusDiv.appendChild(myPlus);
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+const myTrash = new Image();
+myTrash.src = imgtrash;
 
-  element.appendChild(myIcon);
+// task class
 
-  return element;
+
+//UI handle UI tasks
+
+class Task {
+  constructor(index, description, completed) {
+    this.index = index;
+    this.description = description;
+    this.completed = completed;
+  }
 }
 
-document.body.appendChild(component());
+class Schedule {
+
+  static displayTasks() {
+    const taskSchedule = [
+      {
+        index: '01',
+        description: 'Write Lecture',
+        completed: false,
+      },
+      {
+        index: '02',
+        description: 'Write Letter',
+        completed: false,
+      },
+    ];
+
+    const tasks = taskSchedule;
+
+    tasks.forEach((task) => Schedule.addTaskToList(task));
+  }
+
+  static addTaskToList(task) {
+    const list = document.querySelector('.task-list');
+    const row = document.createElement('div');
+    row.appendChild(myTrash);
+    row.draggable = true;
+    row.style.display='flex';
+    row.style.justifyContent="space-between";
+    row.style.borderBottom="2px"
+
+    row.innerHTML = `
+      <input type="checkbox" id="check">
+      <p>${task.description}</p>
+      <img class="remove" src="${imgtrash}" alt="reload">
+      `;
+    list.appendChild(row);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', Schedule.displayTasks);
