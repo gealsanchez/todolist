6abec38a1b252854d40fa43d5aea66ca83c31f7a
gealsanchez <gealsanchez@gmail.com>
2022-11-16 16:03:19 -0500

    Testing with Jest

---
+++ src/test/todolist.test.js
@@ -0,0 +1,18 @@
+import 'jest-localstorage-mock';
+import Storage from '../module/classStorage';
+import Task from '../module/classTask';
+
+describe('Test methods', () => {
+    test('add task to the list', () => {
+        const task = new Task(1, 'tasking', false);
+        Storage.addTask(task);
+        const tasks = Storage.getTasks();
+        expect(tasks.length).toBe(1);
+    });
+
+    test('delete task from the list', () => {
+      Storage.removeTask(1);
+      const tasks = Storage.getTasks();
+      expect(tasks.length).toBe(0);
+    });
+});
