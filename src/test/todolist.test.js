2f37bd4cd2338f7469283184580ce71f3a3c27a1
gealsanchez <gealsanchez@gmail.com>
2023-01-26 15:19:09 -0500

    Edit test updated

--- src/test/todolist.test.js
+++ src/test/todolist.test.js
@@ -9,6 +9,25 @@ describe('Test methods', () => {
     const tasks = Storage.getTasks();
     expect(tasks.length).toBe(1);
   });
+  test('edit task from the list', () => {
+    document.body.innerHTML = '<div class="task-list"<>' +
+      '</div>';
+    Storage.editTask(1, 'driving');
+    const tasks = Storage.getTasks();
+    expect(tasks[0].description).toBe('driving');
+  });
+
+  test('edit completion from a task', () => {
+    Storage.editCompletion(1, true);
+    const tasks = Storage.getTasks();
+    expect(tasks[0].completed).toBe(true);
+  });
+
+  test('clear all checked tasks', () => {
+    Storage.clearAllCompleted(1, true);
+    const tasks = Storage.getTasks();
+    expect(tasks.length).toBe(0);
+  });
 
   test('delete task from the list', () => {
     Storage.removeTask(1);
